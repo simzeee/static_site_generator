4a523bfd35ae3a9d6f83668446a906c260986d44
simzeee <johnsims.dev@gmail.com>
2025-04-21 15:29:41 -0400

    add LeafNode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,9 +1,17 @@
+from collections.abc import Mapping
+
+
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = [] if children is None else children
-        self.props = {} if props is None else props
+        if props is not None and not isinstance(props, Mapping):
+            raise TypeError(
+                f"props must be a dict-like Mapping, got {type(props).__name__}"
+            )
+        else:
+            self.props = {} if props is None else props
 
     def to_html(self):
         raise NotImplementedError
@@ -21,3 +29,18 @@ class HTMLNode:
     def __repr__(self):
         children_repr = f"[{len(self.children)} children]" if self.children else "[]"
         return f"HTMLNODE(tag={self.tag}, value={self.value}, children={children_repr}, props={self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        if value is None:
+            raise ValueError("LeafNode must have a value")
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            return str(self.value)
+
+        props_str = self.props_to_html()
+
+        return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
---
+++ src/test_leafnode.py
@@ -0,0 +1,36 @@
+import unittest
+from htmlnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_no_tag(self):
+        # Test when tag is None
+        node = LeafNode(None, "Just some text")
+        self.assertEqual(node.to_html(), "Just some text")
+
+    def test_leaf_to_html_with_props(self):
+        # Test with properties
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_leaf_to_html_multiple_props(self):
+        # Test with multiple properties
+        node = LeafNode(
+            "img", "An image", {"src": "image.jpg", "alt": "An image", "width": 100}
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<img src="image.jpg" alt="An image" width="100">An image</img>',
+        )
+
+    def test_leaf_node_value_none(self):
+        # Test that ValueError is raised when value is None
+        with self.assertRaises(ValueError):
+            LeafNode("p", None)
